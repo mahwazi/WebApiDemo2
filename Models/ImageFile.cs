18e87df75cfb4577f980b117de741d60d0c92107
Mohammad Ahmadi <mahmadi@rangeresources.com>
2020-05-11 12:28:39 -0500

    Add project files.

---
+++ Models/ImageFile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiDemo2.Models
+{
+    public class ImageFile
+    {
+        // file name
+
+        public string FileName { get; set; }
+
+                              // file content 
+        public string FileContent { get; set; }
+    }
+}
